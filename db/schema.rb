43650e8f7983724d4ade43962212cf523586e551
y-kai86 <y.kai486@gmail.com>
2020-08-07 14:09:21 +0900

    ニックネームの表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_025921) do
+ActiveRecord::Schema.define(version: 2020_08_07_045900) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", null: false
